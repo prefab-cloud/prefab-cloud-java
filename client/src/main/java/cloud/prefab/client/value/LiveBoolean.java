01cd0630bcb40cb29bfa3a4353b65454e10845e9
James Kebinger <james.kebinger@prefab.cloud>
2023-02-28 15:12:43 -0600

    Adds a logged warning if the typed Live value is used with a config of a different type

--- client/src/main/java/cloud/prefab/client/value/LiveBoolean.java
+++ client/src/main/java/cloud/prefab/client/value/LiveBoolean.java
@@ -2,11 +2,13 @@ package cloud.prefab.client.value;
 
 import cloud.prefab.client.ConfigClient;
 import cloud.prefab.domain.Prefab;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 public class LiveBoolean extends AbstractLiveValue<Boolean> {
+  private static final Logger LOG = LoggerFactory.getLogger(LiveBoolean.class);
 
   public LiveBoolean(ConfigClient configClient, String key) {
     super(configClient, key);
@@ -17,6 +19,7 @@ public class LiveBoolean extends AbstractLiveValue<Boolean> {
     if (value.hasBool()) {
       return Optional.of(value.getBool());
     } else {
+      LOG.warn(String.format("Config value for key '%s' used as a boolean does not have a boolean value, will treat as empty", key));
       return Optional.empty();
     }
   }
--- client/src/main/java/cloud/prefab/client/value/LiveString.java
+++ client/src/main/java/cloud/prefab/client/value/LiveString.java
@@ -2,9 +2,14 @@ package cloud.prefab.client.value;
 
 import cloud.prefab.client.ConfigClient;
 import cloud.prefab.domain.Prefab;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Optional;
 
 public class LiveString extends AbstractLiveValue<String> {
+  private static final Logger LOG = LoggerFactory.getLogger(LiveString.class);
+
 
   public LiveString(ConfigClient configClient, String key) {
     super(configClient, key);
@@ -15,6 +20,7 @@ public class LiveString extends AbstractLiveValue<String> {
     if (value.hasString()) {
       return Optional.of(value.getString());
     } else {
+      LOG.warn(String.format("Config value for key '%s' used as a string does not have a string value, will treat as empty", key));
       return Optional.empty();
     }
   }
