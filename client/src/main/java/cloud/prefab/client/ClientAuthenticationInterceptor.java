f4d170e1b51045d72c4e829da4b0a54857934861
James Kebinger <james.kebinger@prefab.cloud>
2023-04-05 17:41:29 -0500

    Expose header names and values - skip auth for GreetingService

--- client/src/main/java/cloud/prefab/client/ClientAuthenticationInterceptor.java
+++ client/src/main/java/cloud/prefab/client/ClientAuthenticationInterceptor.java
@@ -1,6 +1,7 @@
 package cloud.prefab.client;
 
 import cloud.prefab.client.util.MavenInfo;
+import cloud.prefab.domain.GreetingServiceGrpc;
 import io.grpc.CallOptions;
 import io.grpc.Channel;
 import io.grpc.ClientCall;
@@ -9,20 +10,24 @@ import io.grpc.ForwardingClientCall;
 import io.grpc.ForwardingClientCallListener;
 import io.grpc.Metadata;
 import io.grpc.MethodDescriptor;
+import java.util.Set;
 
 public class ClientAuthenticationInterceptor implements ClientInterceptor {
 
-  public static final Metadata.Key<String> CUSTOM_HEADER_KEY = Metadata.Key.of(
-    "auth",
+  public static final String CUSTOM_HEADER_KEY = "auth";
+  public static final Metadata.Key<String> CUSTOM_HEADER_METADATA_KEY = Metadata.Key.of(
+    CUSTOM_HEADER_KEY,
     Metadata.ASCII_STRING_MARSHALLER
   );
 
-  public static final Metadata.Key<String> CLIENT_HEADER_KEY = Metadata.Key.of(
+  public static final String CLIENT_HEADER_KEY = "client";
+
+  public static final Metadata.Key<String> CLIENT_HEADER_METADATA_KEY = Metadata.Key.of(
     "client",
     Metadata.ASCII_STRING_MARSHALLER
   );
 
-  private static final String CLIENT_HEADER_VALUE = String.format(
+  public static final String CLIENT_HEADER_VALUE = String.format(
     "%s.%s",
     MavenInfo.getInstance().getArtifactId(),
     MavenInfo.getInstance().getVersion()
@@ -34,21 +39,27 @@ public class ClientAuthenticationInterceptor implements ClientInterceptor {
     this.apikey = apikey;
   }
 
+  private static final Set<String> NO_AUTH_SERVICE_NAMES = Set.of(
+    GreetingServiceGrpc.SERVICE_NAME
+  );
+
   @Override
   public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
     MethodDescriptor<ReqT, RespT> methodDescriptor,
     CallOptions callOptions,
     Channel channel
   ) {
-    return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(
+    return new ForwardingClientCall.SimpleForwardingClientCall<>(
       channel.newCall(methodDescriptor, callOptions)
     ) {
       @Override
       public void start(Listener<RespT> responseListener, Metadata headers) {
-        headers.put(CLIENT_HEADER_KEY, CLIENT_HEADER_VALUE);
-        headers.put(CUSTOM_HEADER_KEY, apikey);
+        headers.put(CLIENT_HEADER_METADATA_KEY, CLIENT_HEADER_VALUE);
+        if (!NO_AUTH_SERVICE_NAMES.contains(methodDescriptor.getServiceName())) {
+          headers.put(CUSTOM_HEADER_METADATA_KEY, apikey);
+        }
         super.start(
-          new ForwardingClientCallListener.SimpleForwardingClientCallListener<RespT>(
+          new ForwardingClientCallListener.SimpleForwardingClientCallListener<>(
             responseListener
           ) {},
           headers
