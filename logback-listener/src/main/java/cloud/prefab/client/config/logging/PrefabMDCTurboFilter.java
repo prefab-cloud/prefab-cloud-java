760ad6f9ac0af6dfa94a049cb48941b4a2357876
James Kebinger <james.kebinger@prefab.cloud>
2023-04-12 16:44:51 -0500

    Extract a BaseTurboFilter from PrefabMDCTurboFilter ready to add a new filter for contexts

--- logback-listener/src/main/java/cloud/prefab/client/config/logging/PrefabMDCTurboFilter.java
+++ logback-listener/src/main/java/cloud/prefab/client/config/logging/PrefabMDCTurboFilter.java
@@ -3,8 +3,6 @@ package cloud.prefab.client.config.logging;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.turbo.TurboFilter;
-import ch.qos.logback.core.spi.FilterReply;
 import cloud.prefab.client.ConfigClient;
 import cloud.prefab.domain.Prefab;
 import java.util.Collections;
@@ -12,17 +10,11 @@ import java.util.Map;
 import java.util.Optional;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.slf4j.Marker;
 
-public class PrefabMDCTurboFilter extends TurboFilter {
-
-  private final ThreadLocal<Boolean> recursionCheck = ThreadLocal.withInitial(() -> false
-  );
-
-  private final ConfigClient configClient;
+public class PrefabMDCTurboFilter extends BaseTurboFilter {
 
   PrefabMDCTurboFilter(ConfigClient configClient) {
-    this.configClient = configClient;
+    super(configClient);
   }
 
   public static void install(ConfigClient configClient) {
@@ -32,46 +24,11 @@ public class PrefabMDCTurboFilter extends TurboFilter {
   }
 
   @Override
-  public FilterReply decide(
-    Marker marker,
-    Logger logger,
-    Level level,
-    String s,
-    Object[] objects,
-    Throwable throwable
-  ) {
-    if (!configClient.isReady()) {
-      return FilterReply.NEUTRAL;
-    }
-    if (recursionCheck.get()) {
-      return FilterReply.NEUTRAL;
-    } else {
-      recursionCheck.set(true);
-    }
-    configClient.reportLoggerUsage(
+  Optional<Prefab.LogLevel> getLogLevel(Logger logger, Level level) {
+    Map<String, String> mdcData = MDC.getCopyOfContextMap();
+    return configClient.getLogLevelFromStringMap(
       logger.getName(),
-      LogbackLevelMapper.REVERSE_LEVEL_MAP.get(level),
-      1
+      mdcData != null ? mdcData : Collections.emptyMap()
     );
-
-    try {
-      Map<String, String> mdcData = MDC.getCopyOfContextMap();
-      Optional<Prefab.LogLevel> loglevelMaybe = configClient.getLogLevelFromStringMap(
-        logger.getName(),
-        mdcData != null ? mdcData : Collections.emptyMap()
-      );
-      if (loglevelMaybe.isPresent()) {
-        Level calculatedMinLogLevelToAccept = LogbackLevelMapper.LEVEL_MAP.get(
-          loglevelMaybe.get()
-        );
-        if (level.isGreaterOrEqual(calculatedMinLogLevelToAccept)) {
-          return FilterReply.ACCEPT;
-        }
-        return FilterReply.DENY;
-      }
-      return FilterReply.NEUTRAL;
-    } finally {
-      recursionCheck.set(false);
-    }
   }
 }
