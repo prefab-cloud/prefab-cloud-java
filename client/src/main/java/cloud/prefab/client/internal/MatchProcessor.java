9a33c3f61b1714c043bdc533d559d86dd05112bb
James Kebinger <james.kebinger@prefab.cloud>
2023-07-11 16:28:52 -0500

    Update MatchProcessor to upload example contexts, handle retries

--- client/src/main/java/cloud/prefab/client/internal/MatchProcessor.java
+++ client/src/main/java/cloud/prefab/client/internal/MatchProcessor.java
@@ -2,7 +2,10 @@ package cloud.prefab.client.internal;
 
 import cloud.prefab.client.Options;
 import cloud.prefab.client.config.Match;
+import cloud.prefab.context.PrefabContextSet;
 import cloud.prefab.domain.Prefab;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import dev.failsafe.Bulkhead;
@@ -12,9 +15,9 @@ import java.net.http.HttpResponse;
 import java.time.Clock;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -38,7 +41,7 @@ public class MatchProcessor {
   private static final int OUTPUT_QUEUE_SIZE = 10;
   private final List<Event> drain = new ArrayList<>(DRAIN_SIZE); // don't allocate a new one every run
 
-  private final LinkedBlockingQueue<MatchStatsAggregator.StatsAggregate> outputQueue = new LinkedBlockingQueue<>(
+  private final LinkedBlockingQueue<OutputBuffer> outputQueue = new LinkedBlockingQueue<>(
     OUTPUT_QUEUE_SIZE
   );
 
@@ -47,11 +50,13 @@ public class MatchProcessor {
   );
 
   private final MatchStatsAggregator matchStatsAggregator = new MatchStatsAggregator();
+  private final ContextDeduplicator contextDeduplicator;
 
   MatchProcessor(Options options, PrefabHttpClient prefabHttpClient, Clock clock) {
     this.options = options;
     this.prefabHttpClient = prefabHttpClient;
     this.clock = clock;
+    this.contextDeduplicator = new ContextDeduplicator();
   }
 
   void start() {
@@ -101,6 +106,7 @@ public class MatchProcessor {
   }
 
   void aggregationLoop() {
+    Set<Prefab.ExampleContext> recentlySeenContexts = new HashSet<>(); //make bounded!
     while (true) {
       try {
         drain.add(matchQueue.take()); //blocks
@@ -109,12 +115,28 @@ public class MatchProcessor {
           if (event.eventType == Event.EventType.MATCH) {
             MatchEvent matchEvent = (MatchEvent) event;
             matchStatsAggregator.recordMatch(matchEvent.match, event.timestamp);
+            PrefabContextSet context = matchEvent.lookupContext.getPrefabContextSet();
+            String fingerPrint = context.getFingerPrint();
+            if (!fingerPrint.isBlank()) {
+              if (!contextDeduplicator.recentlySeen(fingerPrint)) {
+                recentlySeenContexts.add(
+                  Prefab.ExampleContext
+                    .newBuilder()
+                    .setTimestamp(event.timestamp)
+                    .setContextSet(PrefabContextSet.convert(context).toProto())
+                    .build()
+                );
+              }
+            }
           }
           if (event.eventType == Event.EventType.FLUSH) {
-            // do the flush!
             MatchStatsAggregator.StatsAggregate stats = matchStatsAggregator.getAndResetStatsAggregate();
-            if (!outputQueue.offer(stats)) {
+            Set<Prefab.ExampleContext> contextsToSend = recentlySeenContexts;
+            recentlySeenContexts = new HashSet<>();
+            if (!outputQueue.offer(new OutputBuffer(contextsToSend, stats))) {
+              //restore state and keep aggregating
               matchStatsAggregator.setStatsAggregate(stats);
+              recentlySeenContexts = contextsToSend;
             }
           }
         }
@@ -141,24 +163,21 @@ public class MatchProcessor {
 
     while (true) {
       try {
-        MatchStatsAggregator.StatsAggregate statsAggregate = outputQueue.take();
-        if (statsAggregate.getCounterData().isEmpty()) {
-          continue;
+        bulkhead.acquirePermit();
+        OutputBuffer outputBuffer = outputQueue.take();
+        Prefab.TelemetryEvents telemetryEvents = outputBuffer.toTelemetryEvents();
+        if (!telemetryEvents.getEventsList().isEmpty()) {
+          LOG.debug("Uploading {}", telemetryEvents);
+          Failsafe
+            .with(retryPolicy)
+            .getStageAsync(() -> prefabHttpClient.reportTelemetryEvents(telemetryEvents))
+            .whenComplete((r, t) -> {
+              // don't care if error or not here, just want to release the permit
+              bulkhead.releasePermit();
+            });
+        } else {
+          bulkhead.releasePermit();
         }
-        Prefab.ConfigEvaluationSummaries proto = statsAggregate.toProto();
-        LOG.debug("Uploading {}", proto);
-
-        Failsafe
-          .with(retryPolicy)
-          .compose(bulkhead)
-          .getStageAsync(() ->
-            prefabHttpClient.reportTelemetryEvents(
-              Prefab.TelemetryEvents
-                .newBuilder()
-                .addEvents(Prefab.TelemetryEvent.newBuilder().setSummaries(proto).build())
-                .build()
-            )
-          );
       } catch (InterruptedException e) {
         // continue
       }
@@ -166,7 +185,6 @@ public class MatchProcessor {
   }
 
   void flushStats() {
-    //TODo log the result here?
     matchQueue.offer(new MatchProcessor.Event(Event.EventType.FLUSH, clock.millis()));
   }
 
@@ -195,6 +213,66 @@ public class MatchProcessor {
       super(EventType.MATCH, timestamp);
       this.match = match;
       this.lookupContext = lookupContext;
+    }
+  }
+
+  private static class ContextDeduplicator {
+
+    private final Cache<String, String> cache;
+
+    ContextDeduplicator() {
+      this.cache =
+        CacheBuilder
+          .newBuilder()
+          .expireAfterWrite(10, TimeUnit.MINUTES)
+          .maximumSize(1000)
+          .build();
+    }
+
+    boolean recentlySeen(String fingerprint) {
+      if (cache.getIfPresent(fingerprint) != null) {
+        return true;
+      }
+      cache.put(fingerprint, fingerprint);
+      return false;
+    }
+  }
+
+  private static class OutputBuffer {
+
+    Set<Prefab.ExampleContext> recentlySeenContexts;
+    MatchStatsAggregator.StatsAggregate statsAggregate;
+
+    public OutputBuffer(
+      Set<Prefab.ExampleContext> recentlySeenContexts,
+      MatchStatsAggregator.StatsAggregate statsAggregate
+    ) {
+      this.recentlySeenContexts = recentlySeenContexts;
+      this.statsAggregate = statsAggregate;
+    }
+
+    Prefab.TelemetryEvents toTelemetryEvents() {
+      Prefab.TelemetryEvents.Builder telemetryEventsBuilder = Prefab.TelemetryEvents.newBuilder();
+      if (!recentlySeenContexts.isEmpty()) {
+        telemetryEventsBuilder.addEvents(
+          Prefab.TelemetryEvent
+            .newBuilder()
+            .setExampleContexts(
+              Prefab.ExampleContexts
+                .newBuilder()
+                .addAllExamples(recentlySeenContexts)
+                .build()
+            )
+            .build()
+        );
+      }
+      if (!statsAggregate.getCounterData().isEmpty()) {
+        telemetryEventsBuilder.addEvents(
+          Prefab.TelemetryEvent.newBuilder().setSummaries(statsAggregate.toProto())
+        );
+      }
+
+      return telemetryEventsBuilder.build();
     }
   }
 }
