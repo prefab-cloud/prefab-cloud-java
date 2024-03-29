a3ac72e20cc61c5a9c822778e37fecc0c7f75e93
James Kebinger <james.kebinger@prefab.cloud>
2024-03-29 14:33:41 -0500

    Adds basic Duration support

---
+++ client/src/main/java/cloud/prefab/client/value/LiveDuration.java
@@ -0,0 +1,32 @@
+package cloud.prefab.client.value;
+
+import cloud.prefab.client.ConfigClient;
+import cloud.prefab.domain.Prefab;
+import java.time.Duration;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LiveDuration extends AbstractLiveValue<Duration> {
+
+  private static final Logger LOG = LoggerFactory.getLogger(LiveDuration.class);
+
+  public LiveDuration(ConfigClient configClient, String key) {
+    super(configClient, key);
+  }
+
+  @Override
+  public Optional<Duration> resolve(Prefab.ConfigValue value) {
+    if (value.hasDuration()) {
+      return Optional.of(Duration.parse(value.getDuration().getDefinition()));
+    } else {
+      LOG.warn(
+        String.format(
+          "Config value for key '%s' used as a long does not have duration value, will treat as empty",
+          key
+        )
+      );
+      return Optional.empty();
+    }
+  }
+}
